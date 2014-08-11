d511198dd55da21421c42a2cd1a0ff251a0a805d
gvh <gruevyhat@gmail.com>
2014-08-12 00:15:07 +0900

    Added all animal tables.

--- traveller/animal.py
+++ traveller/animal.py
@@ -3,17 +3,17 @@
 
 # UNDER CONSTRUCTION
 
-
-from dice import d6, d3
+from attributes import SkillSet, Stats
+from dice import d6, d3, d100
 
 
 TERRAIN_DM = {
     # Terrain Type: (DM, Size DM, (1, 2, 3, 4, 5, 6))
-    "Clear": (3, None, (("W", 0), ("W", 0), ("W", 0), ("W", 0), ("W", 2), ("F", -6))),
-    "Plain/Prairie": (4, None, (("W", 0), ("W", 0), ("W", 0), ("W", 2), ("W", 4), ("F", -6))),
+    "Clear": (3, 0, (("W", 0), ("W", 0), ("W", 0), ("W", 0), ("W", 2), ("F", -6))),
+    "Plain/Prairie": (4, 0, (("W", 0), ("W", 0), ("W", 0), ("W", 2), ("W", 4), ("F", -6))),
     "Desert": (3, -3, (("W", 0), ("W", 0), ("W", 0), ("W", 0), ("F", -4), ("F", -6))),
-    "Hills": (None, None, (("W", 0), ("W", 0), ("W", 0), ("W", 2), ("F", -4), ("F", -6))),
-    "Mountain": (None, None, (("W", 0), ("W", 0), ("W", 0), ("F", -2), ("F", -4), ("F", -6))),
+    "Hills": (0, 0, (("W", 0), ("W", 0), ("W", 0), ("W", 2), ("F", -4), ("F", -6))),
+    "Mountain": (0, 0, (("W", 0), ("W", 0), ("W", 0), ("F", -2), ("F", -4), ("F", -6))),
     "Forest": (-4, -4, (("W", 0), ("W", 0), ("W", 0), ("W", 0), ("F", -4), ("F", -6))),
     "Woods": (-2, -1, (("W", 0), ("W", 0), ("W", 0), ("W", 0), ("W", 0), ("F", -6))),
     "Jungle": (-4, -3, (("W", 0), ("W", 0), ("W", 0), ("W", 0), ("W", 2), ("F", -6))),
@@ -62,7 +62,6 @@ SIZE = {
     }
 
 WEAPONS = {
-    # 2d6 Weapons 2d6 Armour
     1: ("None", 0),
     2: ("Teeth", 0),
     3: ("Horns", 0),
@@ -87,6 +86,93 @@ def damage(x):
 
 def encountered(pack):
     size = {0: 1, 1: d3(1), 3: d6(1), 6: d6(2), 9: d6(3), 12: d6(4), 15: d6(5)}
-    for s, n in size.items():
+    for s, n in sorted(size.items(), reverse=True):
         if pack >= s:
             return n
+
+
+QUIRKS = {
+    "Sensory Quirk": ("Sees in infrared",
+                      "Multiple eyes ",
+                      "Sensitive cilia or hairs",
+                      "Echolocation",
+                      "Symbiosis with a hunter or pilot animal",
+                      "Sensitive sense of smell"),
+    "Defence Quirk": ("Armoured shell",
+                      "Screeches for aid ",
+                      "Reacts to attack with attack",
+                      "Camouflage",
+                      "Avoids or mitigates attacks ",
+                      "Inflates"),
+    "Nesting Quirk": ("Digs burrows",
+                      "Nests in trees",
+                      "Hides stocks of food",
+                      "Steals nests",
+                      "Builds nests from organic material",
+                      "Fortifies nests"),
+    "Locomotion Quirk": ("Six limbs",
+                         "Gasbags",
+                         "Crawls on webbing",
+                         "Slime",
+                         "Hijacks the bodies of other creatures",
+                         "A prehensile body part not normally prehensile"),
+    "Reproduction Quirk": ("Lays eggs",
+                           "Lays thousands of spawn",
+                           "Males gestate young",
+                           "Multiple sexes",
+                           "Young grow inside adult, released on death",
+                           "Can change gender"),
+    "Attack Quirk": ("Acid spit",
+                     "Strangling tentacles",
+                     "Primitive tools",
+                     "Spiked tail or bill",
+                     "Electricity",
+                     "Poison"),
+    }
+
+BEHAVIORS = {
+    "Carrion-Eater": (("Instinct", 2)),
+    "Chaser": (("Dexterity", 4), ("Instinct", 2), ("Pack", 2)),
+    "Eater": (("Endurance", 4), ("Pack", 2)),
+    "Filter": (("Endurance", 4)),
+    "Gatherer": (("Stealth", 0), ("Pack", 2)),
+    "Grazer": (("Instinct", 2), ("Pack", 4)),
+    "Hunter": (("Survival", 0), ("Instinct", 2)),
+    "Hijacker": (("Strength", 2), ("Pack", 2)),
+    "Intimidator": (("Persuade", 0)),
+    "Killer, Brute": (("Melee", 0), ("Strength", 4), ("Instinct", 4), ("Pack", -2)),
+    "Killer, Swift": (("Melee", 0), ("Dexterity", 4), ("Instinct", 4), ("Pack", -2)),
+    "Intermittent": (("Pack", 4)),
+    "Pouncer": (("Stealth", 0), ("Recon", 0), ("Athletics", 0), ("Dexterity", 4), ("Instinct", 4)),
+    "Reducer": (("Pack", 4)),
+    "Siren": (("Pack", -4)),
+    "Trapper": (("Pack", -2)),
+    }
+
+
+CREATURE = {
+    "Scavenger": (11.1, 0),
+    "Omnivore": (38.86, 4),
+    "Herbivore": (83.28, 8),
+    "Carnivore": (100, -6)
+    }
+
+#   Base attrib
+#   -> Str, Dex, End, Int, Instinct, Pack
+#   Base skills
+#   -> Survival 0, Athletics 0, Recon 0, Melee (natural weapons) 0
+#   Rand Creature type
+#   Rand Terrain type
+#   -> mvmt d6(1)
+#       -> size d6(2) + Terrain + Mvmt
+#   Animal type d6(2) + Terrain
+#   -> behavior skills
+#   -> +d6(1) skills
+#   Sample 1d6 (w/repl) quirks
+
+
+class Animal(object):
+
+    def __init__(self):
+        self.stats = Stats(animal=True)
+        self.skills = SkillSet()
